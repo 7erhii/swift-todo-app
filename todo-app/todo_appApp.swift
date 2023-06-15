81deb0e37db769104de12269b056c8eb48518d2f
7erhiy <7erhiy@gmail.com>
2023-06-15 11:58:51 +0300

    Initial Commit

---
+++ todo-app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo-app
+//
+//  Created by Sehiy Krepets on 15.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
